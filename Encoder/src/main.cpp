ff8d8108900aa852a56b0330fda020cbae6bd801
Emil Asgeirsson <Emil.asgeirsson@gmail.com>
2023-04-25 16:18:14 +0200

    updated GUI

--- Encoder/src/main.cpp
+++ Encoder/src/main.cpp
@@ -17,9 +17,9 @@ constexpr uint8_t decimals = 2;
 
 void loop()
 {
-  const float value_a0 = analogRead(PIN_A0) * to_angle;
-  const float value_a1 = analogRead(PIN_A1) * to_angle;
-  const float value_a2 = analogRead(PIN_A2) * to_angle;
+  const float value_a0 = analogRead(PIN_A0); // Pressure
+  const float value_a1 = analogRead(PIN_A1) * to_angle; // temp encoder
+  const float value_a2 = analogRead(PIN_A2) * to_angle; // encoder
   Serial.print(String(value_a0, decimals) + ',' +
                String(value_a1, decimals) + ',' +
                String(value_a2, decimals) + '\n');
