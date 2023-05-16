ebc4921b7ed47f7c3f52f5f1d8ea11d765fda59b
ibyou1997 <62427592+ibyou1997@users.noreply.github.com>
2023-05-16 09:06:55 +0200

    Update Force_calc.py

--- Force_calc.py
+++ Force_calc.py
@@ -92,10 +92,11 @@ if __name__ == '__main__':
     prev_angle = 1
     pressure = 6
     dt = 0.01
+    
     force = calculate_force_and_power(angle, pressure, True)
     work, power = calc_power(angle, prev_angle, dt, pressure, True)
     print("Angle: ", angle, "degrees")
-    print("Force: ", force, "N")
-    print("Work: ", work, "J")
-    print("Power: ", power, "W")
+    #print("Force: ", force, "N")
+    #print("Work: ", work, "J")
+    #print("Power: ", power, "W")
 
